41128b4681922da6b1168f713935f68a9eaf537e
Toru Hisai <toru@torus.jp>
2017-08-12 17:24:00 +0000

    test

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+.PHONY: test
+
 init:
 	npm install
 
---
+++ test/hello.js
@@ -0,0 +1,7 @@
+var should = require('should')
+
+describe('Hello', function() {
+  it('greetings', function() {
+    (5).should.be.exactly(5).and.be.a.Number()
+  })
+})
