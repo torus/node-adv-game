e641eb3bc1717b0c71ef1e4e50ecd330539bd9e8
Toru Hisai <toru@torus.jp>
2017-08-30 16:26:16 +0000

    async test

--- test/async.js
+++ test/async.js
@@ -18,4 +18,52 @@ describe('async/await', function() {
       done()
     })
   })
+
+  it('throws!', function(done) {
+    var oneMoment = new Promise(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, 100);
+    })
+
+    var f = async () => {
+      await oneMoment;
+
+      throw "woo!";
+      return "nee";
+    }
+
+    var g = async () => {
+      var result = null;
+      var err = null;
+      try {
+        result = await f();
+      } catch (e) {
+        err = e;
+      }
+      should.not.exist(result);
+      err.should.be.equal('woo!');
+    }
+
+    g().then(done)
+  })
+
+  it('receives an exeption from a Promise', done => {
+    var pitcher = new Promise((resolve, reject) => {
+      return reject("ball");
+    })
+
+    var f = async () => {
+      var result;
+      try {
+        result = await pitcher;
+      } catch (e) {
+        result = e;
+      }
+
+      result.should.be.equal("ball");
+    }
+
+    f().then(done);
+  })
 })
