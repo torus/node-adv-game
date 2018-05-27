2509d4c75e5ffbb928e2ce652bdcfa52a1edb238
Toru Hisai <toru@torus.jp>
2018-05-27 21:51:12 +0200

    desc and speak in action

--- static/script.js
+++ static/script.js
@@ -1,22 +1,25 @@
 $(document).ready(function() {
   var elems = $('.adv-one-by-one')
   var options = $('.adv-options')
-  elems.hide()
-  options.hide()
 
-  elems.each(function (i) {
-    console.log(i)
-    ;(function(i, e){
-      e.on('click', function() {
-        console.log('clicked', i)
-        if (i != elems.length - 1) {
-          e.hide()
-          $(elems[i + 1]).show()
-        } else {
-          options.show()
-        }
-      })
-    })(i, $(this))
-  })
-  $(elems[0]).show()
+  if (elems.length > 1) {
+    elems.hide()
+    options.hide()
+
+    elems.each(function (i) {
+      ;(function(i, e){
+        e.on('click', function() {
+          console.log('clicked', i)
+          if (i != elems.length - 1) {
+            e.hide()
+            $(elems[i + 1]).show()
+            if (i == elems.length - 2) {
+              options.show()
+            }
+          }
+        })
+      })(i, $(this))
+    })
+    $(elems[0]).show()
+  }
 })
