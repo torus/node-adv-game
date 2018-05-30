bc5e3861de68a7538436a5c7f2f5d995150e811d
Toru Hisai <toru@torus.jp>
2018-05-31 01:26:39 +0200

    cosmetics change

--- static/script.js
+++ static/script.js
@@ -1,23 +1,27 @@
 $(document).ready(function() {
+  var nextButton = $('.adv-next-button-row')
   var elems = $('.adv-one-by-one')
-  var options = $('.adv-options')
+  nextButton.hide()
 
   if (elems.length > 1) {
+    var options = $('.adv-options')
+
     elems.hide()
     options.hide()
+    nextButton.show()
 
-    elems.each(function (i) {
-      ;(function(i, e){
-        e.on('click', function() {
-          console.log('clicked', i)
-          if (i != elems.length - 1) {
-            $(elems[i + 1]).show()
-            if (i == elems.length - 2) {
-              options.show()
-            }
-          }
-        })
-      })(i, $(this))
+    var i = 0
+    $('.adv-next-button-col button').on('click', function() {
+      console.log('clicked', i)
+      if (i != elems.length - 1) {
+        $(elems[i + 1]).show()
+        if (i == elems.length - 2) {
+          options.show()
+          nextButton.hide()
+        } else {
+          i ++
+        }
+      }
     })
     $(elems[0]).show()
   }
