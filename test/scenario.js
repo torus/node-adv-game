fb03ffed22026d40f44b5099e1e14c423aba4cc3
Toru Hisai <toru@torus.jp>
2018-07-18 20:37:52 +0200

    refactoring

--- test/scenario.js
+++ test/scenario.js
@@ -20,3 +20,23 @@ describe('scenario', () => {
     })
   })
 })
+
+describe('sceneCommands', () => {
+  const sceneCommands = require('../lib/commands/sceneCommands')()
+  describe('scene', () => {
+    it('returns scene object', () => {
+      const scn = sceneCommands.scene()
+      scn.should.have.property('display')
+    })
+
+    it('takes functions and they are called', () => {
+      let called = 0
+      const f = s => {
+        called ++
+      }
+      const scn = sceneCommands.scene(f, f, f)
+      scn.should.have.property('display')
+      called.should.be.equal(3)
+    })
+  })
+})
