ed5beea23570fed6a70dc2eddac85bbb0de2753b
Toru Hisai <toru@torus.jp>
2018-07-19 21:53:46 +0200

    tests...

--- test/scenario.js
+++ test/scenario.js
@@ -22,10 +22,10 @@ describe('scenario', () => {
 })
 
 describe('sceneCommands', () => {
-  const sceneCommands = require('../lib/commands/sceneCommands')()
+  const sceneCommands = require('../lib/commands/sceneCommands')
   describe('scene', () => {
     it('returns scene object', () => {
-      const scn = sceneCommands.scene()
+      const scn = sceneCommands(null).scene()
       scn.should.have.property('display')
     })
 
@@ -34,7 +34,7 @@ describe('sceneCommands', () => {
       const f = s => {
         called ++
       }
-      const scn = sceneCommands.scene(f, f, f)
+      const scn = sceneCommands(null).scene(f, f, f)
       scn.should.have.property('display')
       called.should.be.equal(3)
     })
@@ -42,7 +42,7 @@ describe('sceneCommands', () => {
 
   describe('name', () => {
     it('names the scene', async () => {
-      const cmd = sceneCommands.name('namae')
+      const cmd = sceneCommands(null).name('namae')
       cmd.should.be.a.Function()
 
       let scn = {}
@@ -59,7 +59,7 @@ describe('sceneCommands', () => {
 
   describe('stage', () => {
     it('specifies the stage ID', async () => {
-      const cmd = sceneCommands.stage('steji')
+      const cmd = sceneCommands(null).stage('steji')
       cmd.should.be.a.Function()
 
       let scn = {}
@@ -76,7 +76,7 @@ describe('sceneCommands', () => {
 
   describe('desc', () => {
     it('shows description', async () => {
-      const cmd = sceneCommands.desc('Lorem ipsum', 'dolor sit amet')
+      const cmd = sceneCommands(null).desc('Lorem ipsum', 'dolor sit amet')
       cmd.should.be.a.Function()
 
       let scn = {}
@@ -103,7 +103,7 @@ describe('sceneCommands', () => {
 
   describe('speak', () => {
     it('adds lines', async () => {
-      const cmd = sceneCommands.speak('acta', 'Lorem ipsum', 'dolor sit amet')
+      const cmd = sceneCommands(null).speak('acta', 'Lorem ipsum', 'dolor sit amet')
       cmd.should.be.a.Function()
 
       let scn = {}
@@ -122,6 +122,48 @@ describe('sceneCommands', () => {
       const prom = func(runtime, null)
       prom.should.be.an.instanceOf(Promise)
       const handler = await prom
+
+      done.should.be.true()
+      handler.should.be.a.Function()
+      should(handler()).be.exactly(undefined)
+    })
+  })
+
+  describe('link', () => {
+    it('adds a link to the scene', async () => {
+      let setCalled = 0
+      const db = {
+        set: async function(session, key, value) {
+          key.should.be.equal('currentScene-dest')
+          value.should.be.equal('anotherscene')
+          setCalled ++
+        }
+      }
+      const commands = sceneCommands(db)
+
+      commands.scene(commands.stage('dest'), commands.name('anotherscene'))
+
+      const cmd = commands.link('anotherscene')
+      cmd.should.be.a.Function()
+
+      let scn = {linkedSceneSet: {}}
+      const func = cmd(scn)
+      func.should.be.a.Function()
+
+      let done = false
+      const runtime = {
+        addInstruction: function(cmd, scene) {
+          cmd.should.be.equal('link')
+          scene.should.be.equal('dest')
+          done = true
+        }
+      }
+      setCalled.should.be.equal(0)
+      const prom = func(runtime, null)
+      prom.should.be.an.instanceOf(Promise)
+
+      const handler = await prom
+      setCalled.should.be.equal(1)
 
       done.should.be.true()
       handler.should.be.a.Function()
