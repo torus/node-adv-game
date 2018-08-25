05e00b3d1aac1cb8bb9ccbfc638c5b4226a2bcd8
Toru Hisai <toru@torus.jp>
2018-08-25 20:02:06 +0200

    seamless action

--- static/script.js
+++ static/script.js
@@ -1,29 +1,34 @@
+var onClick = null
+
 $(document).ready(function() {
   var nextButton = $('.adv-next-button-row')
   var elems = $('.adv-one-by-one')
   nextButton.hide()
 
-  if (elems.length > 1) {
     var options = $('.adv-options')
 
+  if (elems.length > 1) {
     elems.hide()
     options.hide()
     nextButton.show()
+  }
 
-    var onClick = function() {
-      console.log('clicked', i)
+    var i = 0
+    onClick = function() {
+      elems = $('.adv-one-by-one')
+      console.log('clicked', i, elems.length)
       if (i != elems.length - 1) {
         $(elems[i + 1]).show()
         if (i == elems.length - 2) {
           options.show()
           nextButton.hide()
         } else {
+          options.hide()
+          nextButton.show()
           i ++
         }
       }
-    }
 
-    var i = 0
     $('.adv-next-button-col button').on('click', onClick)
     $('body').on('keydown', function(ev) {
       if (ev.key == " ")
@@ -33,3 +38,33 @@ $(document).ready(function() {
     $(elems[0]).show()
   }
 })
+
+function doAction(actionId) {
+  console.log('doAction', actionId)
+  var nextButton = $('.adv-next-button-row')
+  var options = $('.adv-options')
+
+  $('#moveList').empty()
+  $('#actionList').empty()
+
+  $.post('/action/' + actionId
+         , null
+         , function(data) {
+           console.log('done')
+           console.log(data)
+           data.disp.forEach(function(html) {
+             var elem = $(html)
+             elem.hide()
+             $('#display-body').append(elem)
+           })
+           if (data.disp.length > 0) {
+             onClick()
+           }
+           data.moves.forEach(function(elem) {$('#moveList').append(elem)})
+           data.actions.forEach(function(elem) {$('#actionList').append(elem)})
+         }
+         , 'json'
+  )
+
+  return false
+}
